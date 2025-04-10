97139d6698f5499bca5ba4b1f755f4e87f4d8562
Julien <julienguigues0712@gmail.com>
2025-04-10 12:10:42 +0200

    initial commit

---
+++ src/main/java/launchcmd/launchcmd/LaunchCommandDataGenerator.java
@@ -0,0 +1,11 @@
+package launchcmd.launchcmd;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class LaunchCommandDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
