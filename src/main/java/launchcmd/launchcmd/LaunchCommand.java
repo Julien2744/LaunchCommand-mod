fa93f1f6879e14c720c30898368608b0eda4bae2
Julien <julienguigues0712@gmail.com>
2025-04-11 11:09:37 +0200

    added translation key

--- src/main/java/launchcmd/launchcmd/LaunchCommand.java
+++ src/main/java/launchcmd/launchcmd/LaunchCommand.java
@@ -7,7 +7,6 @@ import net.fabricmc.fabric.api.command.v2.CommandRegistrationCallback;
 import net.minecraft.command.argument.EntityArgumentType;
 import net.minecraft.entity.Entity;
 
-import net.minecraft.entity.LivingEntity;
 import net.minecraft.server.command.CommandManager;
 import net.minecraft.server.command.ServerCommandSource;
 
@@ -73,42 +72,61 @@ public class LaunchCommand implements ModInitializer {
 		});
 	}
 
-	private static int launchAddMotion(ServerCommandSource source, Collection<? extends Entity> entityToLaunch, double motX, double motY, double motZ) {
-		Iterator entitiesCollection = entityToLaunch.iterator();
+	private static int launchAddMotion(ServerCommandSource source, Collection<? extends Entity> entitiesToLaunch, double motX, double motY, double motZ) {
+		Iterator entitiesCollection = entitiesToLaunch.iterator();
 
 		while (entitiesCollection.hasNext()) {
 			Entity target = (Entity) entitiesCollection.next();
-			//source.sendFeedback(() -> Text.of("Add motion to " + ((LivingEntity)target).getName() + "with x:" + motX + " y:" + motY + " z:" + motZ), false);
 			target.addVelocity(motX, motY, motZ);
 			target.velocityModified = true;
 		}
 
-		return 1;
-	}
-
-	private static int launchSetMotion(ServerCommandSource source, Collection<? extends Entity> entityToLaunch, double motX, double motY, double motZ) {
-		Iterator entitiesCollection = entityToLaunch.iterator();
-
-		while (entitiesCollection.hasNext()) {
-			Entity target = (Entity) entitiesCollection.next();
-			//source.sendFeedback(() -> Text.of("Set motion to " + ((LivingEntity)target).getName() + "with x:" + motX + " y:" + motY + " z:" + motZ), false);
-			target.setVelocity(motX, motY, motZ);
-			target.velocityModified = true;
+		if(entitiesToLaunch.size() == 1) {
+			source.sendFeedback(() -> {
+				return Text.translatable("commands.launchcmd.launchadd.success.single", new Object[]{((Entity)entitiesToLaunch.iterator().next()).getDisplayName()});
+			}, true);
+		}
+		else {
+			source.sendFeedback(() -> {
+				return Text.translatable("commands.launchcmd.launchadd.success.multiple",  new Object[]{entitiesToLaunch.size()});
+			}, true);
 		}
 
 		return 1;
 	}
 
-	private static int launchLooking(ServerCommandSource source, Collection<? extends Entity> entityToLaunch, double force) {
+	private static int launchSetMotion(ServerCommandSource source, Collection<? extends Entity> entitiesToLaunch, double motX, double motY, double motZ) {
+		Iterator entitiesCollection = entitiesToLaunch.iterator();
+
+		while (entitiesCollection.hasNext()) {
+			Entity target = (Entity) entitiesCollection.next();
+			target.setVelocity(motX, motY, motZ);
+			target.velocityModified = true;
+		}
+
+		if(entitiesToLaunch.size() == 1) {
+			source.sendFeedback(() -> {
+				return Text.translatable("commands.launchcmd.launchset.success.single", new Object[]{((Entity)entitiesToLaunch.iterator().next()).getDisplayName()});
+			}, true);
+		}
+		else {
+			source.sendFeedback(() -> {
+				return Text.translatable("commands.launchcmd.launchset.success.multiple",  new Object[]{entitiesToLaunch.size()});
+			}, true);
+		}
+
+		return 1;
+	}
+
+	private static int launchLooking(ServerCommandSource source, Collection<? extends Entity> entitiesToLaunch, double force) {
 		//angle is in degree, we need to convert it to radian
 		float sourcePitch = (float)((-(source.getRotation().x) * (Math.PI / 180.0f)) + Math.PI / 2);
 		float sourceYaw = (float)((source.getRotation().y * (Math.PI / 180.0f)) + Math.PI / 2);
 
-		Iterator entitiesCollection = entityToLaunch.iterator();
+		Iterator entitiesCollection = entitiesToLaunch.iterator();
 
 		while (entitiesCollection.hasNext()) {
 			Entity target = (Entity) entitiesCollection.next();
-			//source.sendFeedback(() -> Text.of("Set motion to " + target.getName() + " with force:" + force + " pitch(x):" + sourceYaw + " yaw(y):" + sourcePitch), false);
 			target.addVelocity(
 					force * (Math.cos(sourceYaw)*Math.sin(sourcePitch)),
 					force * Math.sin(sourcePitch - Math.PI / 2),
@@ -117,6 +135,17 @@ public class LaunchCommand implements ModInitializer {
 			target.velocityModified = true;
 		}
 
-		return 1;
+		if(entitiesToLaunch.size() == 1) {
+			source.sendFeedback(() -> {
+				return Text.translatable("commands.launchcmd.launchadd.success.single", new Object[]{((Entity)entitiesToLaunch.iterator().next()).getDisplayName()});
+			}, true);
+		}
+		else {
+			source.sendFeedback(() -> {
+				return Text.translatable("commands.launchcmd.launchadd.success.multiple",  new Object[]{entitiesToLaunch.size()});
+			}, true);
+		}
+
+		return entitiesToLaunch.size();
 	}
 }
