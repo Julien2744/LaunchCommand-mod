79d18559d464d9cdfb7337c30e39eaa4f09904b6
Julien <146923742+Julien2744@users.noreply.github.com>
2025-08-02 16:08:52 +0200

    ported the mod to NeoForge

---
+++ src/main/java/launchcmd/launchcmd/LaunchCommand.java
@@ -0,0 +1,49 @@
+package launchcmd.launchcmd;
+
+import net.neoforged.neoforge.event.RegisterCommandsEvent;
+import org.slf4j.Logger;
+
+import com.mojang.logging.LogUtils;
+
+import net.neoforged.bus.api.IEventBus;
+import net.neoforged.bus.api.SubscribeEvent;
+import net.neoforged.fml.common.Mod;
+import net.neoforged.fml.ModContainer;
+import net.neoforged.neoforge.common.NeoForge;
+import net.neoforged.neoforge.event.server.ServerStartingEvent;
+
+
+// The value here should match an entry in the META-INF/neoforge.mods.toml file
+@Mod(LaunchCommand.MOD_ID)
+public class LaunchCommand {
+    // Define mod id in a common place for everything to reference
+    public static final String MOD_ID = "launchcommand";
+    // Directly reference a slf4j logger
+    public static final Logger LOGGER = LogUtils.getLogger();
+    // The constructor for the mod class is the first code that is run when your mod is loaded.
+    // FML will recognize some parameter types like IEventBus or ModContainer and pass them in automatically.
+    public LaunchCommand(IEventBus modEventBus, ModContainer modContainer) {
+
+        // Register ourselves for server and other game events we are interested in.
+        // Note that this is necessary if and only if we want *this* class (ExampleMod) to respond directly to events.
+        // Do not add this line if there are no @SubscribeEvent-annotated functions in this class, like onServerStarting() below.
+        NeoForge.EVENT_BUS.register(this);
+
+        // Register the command
+        NeoForge.EVENT_BUS.addListener(this::registerCommand);
+
+    }
+
+    private void registerCommand(final RegisterCommandsEvent event) {
+        command.register(event.getDispatcher());
+    }
+
+
+    // You can use SubscribeEvent and let the Event Bus discover methods to call
+    @SubscribeEvent
+    public void onServerStarting(ServerStartingEvent event) {
+        // even though it's empty, it's required for the mod to launch somehow
+    }
+
+
+}
---
+++ src/main/java/launchcmd/launchcmd/command.java
@@ -0,0 +1,144 @@
+package launchcmd.launchcmd;
+
+import com.mojang.brigadier.CommandDispatcher;
+import com.mojang.brigadier.arguments.DoubleArgumentType;
+import net.minecraft.commands.CommandSourceStack;
+import net.minecraft.commands.Commands;
+import net.minecraft.commands.arguments.EntityArgument;
+import net.minecraft.network.chat.Component;
+import net.minecraft.world.entity.Entity;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.phys.Vec3;
+
+import java.util.Collection;
+
+public class command {
+    public static void register(CommandDispatcher<CommandSourceStack> dispatcher) {
+        dispatcher.register (
+                Commands.literal("launch")
+                        .requires(source -> source.hasPermission(2))
+                        .then(
+                                Commands.argument("targets", EntityArgument.entities())
+                                        .then(
+                                                Commands.literal("addMotion")
+                                                        .then(
+                                                                Commands.argument("motionX", DoubleArgumentType.doubleArg())
+                                                                        .then(
+                                                                                Commands.argument("motionY", DoubleArgumentType.doubleArg())
+                                                                                        .then(
+                                                                                                Commands.argument("motionZ", DoubleArgumentType.doubleArg())
+                                                                                                        .executes(context -> launchAddMotion(
+                                                                                                                context.getSource(),
+                                                                                                                EntityArgument.getEntities(context, "targets"),
+                                                                                                                DoubleArgumentType.getDouble(context, "motionX"),
+                                                                                                                DoubleArgumentType.getDouble(context, "motionY"),
+                                                                                                                DoubleArgumentType.getDouble(context, "motionZ"))
+
+                                                                                                        )
+                                                                                        )
+                                                                        )
+                                                        )
+                                        )
+                                        .then(
+                                                Commands.literal("setMotion")
+                                                        .then(
+                                                                Commands.argument("motionX", DoubleArgumentType.doubleArg())
+                                                                        .then(
+                                                                                Commands.argument("motionY", DoubleArgumentType.doubleArg())
+                                                                                        .then(
+                                                                                                Commands.argument("motionZ", DoubleArgumentType.doubleArg())
+                                                                                                        .executes(context -> launchSetMotion(
+                                                                                                                context.getSource(),
+                                                                                                                EntityArgument.getEntities(context, "targets"),
+                                                                                                                DoubleArgumentType.getDouble(context, "motionX"),
+                                                                                                                DoubleArgumentType.getDouble(context, "motionY"),
+                                                                                                                DoubleArgumentType.getDouble(context, "motionZ"))
+
+                                                                                                        )
+                                                                                        )
+                                                                        )
+                                                        )
+                                        )
+                                        .then(
+                                                Commands.literal("looking")
+                                                        .then(
+                                                                Commands.argument("force", DoubleArgumentType.doubleArg())
+                                                                        .executes(context -> launchLooking(
+                                                                                context.getSource(),
+                                                                                EntityArgument.getEntities(context, "targets"),
+                                                                                DoubleArgumentType.getDouble(context, "force")
+                                                                        ))
+                                                        )
+
+                                        )
+                        )
+        );
+    }
+
+
+
+    private static int launchAddMotion(CommandSourceStack source, Collection<? extends Entity> entitiesToLaunch, double motX, double motY, double motZ) {
+        for (Entity entity : entitiesToLaunch) {
+            Vec3 motion = new Vec3(motX, motY, motZ);
+            entity.addDeltaMovement(motion);
+            //fix player not being affected
+            if (entity instanceof Player) {
+                entity.hurtMarked = true;
+            }
+        }
+
+        if(entitiesToLaunch.size() == 1) {
+            source.sendSuccess(() -> Component.translatable("commands.launchcmd.launchadd.success.single", entitiesToLaunch.iterator().next().getDisplayName()), true);
+        }
+        else {
+            source.sendSuccess(() -> Component.translatable("commands.launchcmd.launchadd.success.multiple", entitiesToLaunch.size()), true);
+        }
+
+        return entitiesToLaunch.size();
+    }
+    private static int launchSetMotion(CommandSourceStack source, Collection<? extends Entity> entitiesToLaunch, double motX, double motY, double motZ) {
+        for (Entity entity : entitiesToLaunch) {
+            entity.setDeltaMovement(motX, motY, motZ);
+            //fix player not being affected
+            if (entity instanceof Player) {
+                entity.hurtMarked = true;
+            }
+        }
+
+        if(entitiesToLaunch.size() == 1) {
+            source.sendSuccess(() -> Component.translatable("commands.launchcmd.launchset.success.single", entitiesToLaunch.iterator().next().getDisplayName()), true);
+        }
+        else {
+            source.sendSuccess(() -> Component.translatable("commands.launchcmd.launchset.success.multiple", entitiesToLaunch.size()), true);
+        }
+
+        return entitiesToLaunch.size();
+    }
+
+    private static int launchLooking(CommandSourceStack source, Collection<? extends Entity> entitiesToLaunch, double force) {
+        //angle is in degree, we need to convert it to radian
+        float sourcePitch = (float)((-(source.getRotation().x) * (Math.PI / 180.0f)) + Math.PI / 2);
+        float sourceYaw = (float)((source.getRotation().y * (Math.PI / 180.0f)) + Math.PI / 2);
+
+        for (Entity entity : entitiesToLaunch) {
+            Vec3 motion = new Vec3(
+                    force * (Math.cos(sourceYaw)*Math.sin(sourcePitch)),
+                    force * Math.sin(sourcePitch - Math.PI / 2),
+                    force * (Math.sin(sourceYaw)*Math.sin(sourcePitch)));
+            entity.addDeltaMovement(motion);
+            //fix player not being affected
+            if (entity instanceof Player) {
+                entity.hurtMarked = true;
+            }
+        }
+
+        if(entitiesToLaunch.size() == 1) {
+            source.sendSuccess(() -> Component.translatable("commands.launchcmd.launchadd.success.single", entitiesToLaunch.iterator().next().getDisplayName()), true);
+        }
+        else {
+            source.sendSuccess(() -> Component.translatable("commands.launchcmd.launchadd.success.multiple", entitiesToLaunch.size()), true);
+        }
+
+        return entitiesToLaunch.size();
+    }
+}
